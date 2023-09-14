62fc94f2b7b993be8f145415014ae5860c41833f
maxivichy <145025835+maxivichy@users.noreply.github.com>
2023-09-14 15:25:33 +0200

    Add files via upload

---
+++ Kalorienverbrauch rechner/script.js
@@ -0,0 +1,4 @@
+document.getElementById("hover-bar-button-end").addEventListener("click", function() {
+    const pageEndElement = document.getElementById("pageEnd");
+    pageEndElement.scrollIntoView({ behavior: "smooth" }); 
+});
